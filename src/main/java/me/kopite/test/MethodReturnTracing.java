20bd59ac2794a4702874d5d5e57a005c8e8691f9
zhouwei <zhouwei18@meituan.com>
2017-06-25 17:38:20 +0800

    add

--- src/main/java/me/kopite/test/MethodReturnTracing.java
+++ src/main/java/me/kopite/test/MethodReturnTracing.java
@@ -9,17 +9,24 @@ import com.sun.btrace.annotations.Kind;
 import com.sun.btrace.annotations.Location;
 import com.sun.btrace.annotations.OnMethod;
 import com.sun.btrace.annotations.Return;
+import com.sun.btrace.annotations.Self;
 
 /**
+ * 打印方法入参及返回值
+ * 
  * Created by zhouwei on 2017-6-21.
  */
 
 @BTrace
 public class MethodReturnTracing {
 
-    @OnMethod(clazz = "com.sankuai.meituan.banma.thrift.haikui.iface.impl.BmHaikuiContractPoiThriftIfaceImpl", method = "getBmHaikuiContractPoiRelListByUtime", location = @Location(Kind.RETURN))
-    public static void onMethodReturn(@Return Object result) {
+    @OnMethod(clazz = "com.sankuai.meituan.banma.thrift.haikui.iface.impl.BmHaikuiContractPoiThriftIfaceImpl", 
+              method = "getBmHaikuiContractPoiRelListByUtime", 
+              location = @Location(Kind.RETURN))
+    public static void onMethodReturn(@Self Object self, int utime, int page, int pageSize, @Return Object result) {
         println(BTraceUtils.Time.timestamp("yyyy-MM-dd HH:mm:ss"));
+        println("method self: " + str(self));
+        println("method params: utime=" + utime + ", page=" + page + ", pageSize=" + pageSize);
         println("method return: " + str(result));
         println("==========================");
     }
