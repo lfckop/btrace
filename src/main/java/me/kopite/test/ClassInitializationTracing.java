13aea53b6e6796fbf97090adae230014a3b37a5a
zhouwei18 <zhouwei18@meituan.com>
2018-11-22 00:41:03 +0800

    add

--- src/main/java/me/kopite/test/ClassInitializationTracing.java
+++ src/main/java/me/kopite/test/ClassInitializationTracing.java
@@ -1,25 +1,42 @@
 package me.kopite.test;
 
+import static com.sun.btrace.BTraceUtils.print;
 import static com.sun.btrace.BTraceUtils.println;
 
+import com.sun.btrace.BTraceUtils;
 import com.sun.btrace.BTraceUtils.Threads;
 import com.sun.btrace.annotations.BTrace;
 import com.sun.btrace.annotations.Kind;
 import com.sun.btrace.annotations.Location;
 import com.sun.btrace.annotations.OnMethod;
 
+/**
+ * 排查类加载最后一步即初始化的问题，追踪类构造器clinit的异常
+ * 
+ * @author zhouwei
+ *
+ */
 @BTrace
 public class ClassInitializationTracing {
 
-    @OnMethod(clazz = "me.kopite.springboot.test.Test", method = "test", location = @Location(Kind.ERROR))
-    public static void onExecuteError(Throwable e) {
-        println("###error");
+    @OnMethod(clazz = "com.sankuai.qcs.dispatch.monitor.util.common.EnviromentUtil$EnvHolder", method = "<clinit>", location = @Location(Kind.ERROR))
+    public static void onError(Throwable e) {
+        print(BTraceUtils.Time.timestamp("yyyy-MM-dd HH:mm:ss"));
+        println("###error###");
+        
         Threads.jstack(e);
+        
+        println("==========================");
     }
 
-    @OnMethod(clazz = "me.kopite.springboot.test.Test", method = "test", location = @Location(Kind.THROW))
-    public static void anyRead2(Throwable e) {
-        println("###throw");
+    @OnMethod(clazz = "com.sankuai.qcs.dispatch.monitor.util.common.EnviromentUtil$EnvHolder", method = "<clinit>", location = @Location(Kind.THROW))
+    public static void onException(Throwable e) {
+        print(BTraceUtils.Time.timestamp("yyyy-MM-dd HH:mm:ss"));
+        println("###throw###");
+        
         Threads.jstack(e);
+        
+        println("==========================");
     }
+   
 }
